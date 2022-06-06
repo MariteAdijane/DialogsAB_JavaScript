c0a8afcc4c957a124bd103959747519e5d49ddbe
MariteAdijane <marite.adijane@inbox.lv>
2022-06-06 21:15:57 +0300

    JS loģiskās vērtības

---
+++ Class07_20220606/JS/script.js
@@ -0,0 +1,28 @@
+// LOĢISKĀS VĒRTĪBAS
+//+, -, *, /, % // alģebriskās operācijas
+//  ||, &&, ! // loģiskais vai, un, nē
+// |, &, ~ // loģiskais pa bitiem
+// <<,>> // nobīdes operācijas
+// >, <=, == // salīdzināšanas operācijas
+// === // identiskas vērtības
+
+//Number(4 != 3)// no loģiskās operācijas tikt līdz skaitlim
+//typeof(true)= Boolean
+//typeoof
+
+
+//Redz tikai konsolē
+console.log(129 || 0);
+
+//Atspoguļojas demo vietā
+document.getElementById("demo").innerHTML = 123 || 0;
+/*
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    }
+*/
+
+
+//X & -> 00000 00000
+//X & -> 0000 000(X)
+//(X % 2) == 1
