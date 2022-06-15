d684071cfc8b27710c100bac90aba3c57f0d6555
MariteAdijane <marite.adijane@inbox.lv>
2022-06-15 20:20:09 +0300

    class10

---
+++ Class10_20220615/script.js
@@ -0,0 +1,28 @@
+var vards_uzvards = "Jānis Bērziņš,";
+var nauda = 15000;
+var pelna_1 = (nauda * 0.0001) *1;
+var pelna_2 = (nauda * 0.0002) *1;
+var pelna_3 = (nauda * 0.0003) *3;
+var pelna_4 = (nauda * 0.0004) *4;
+var pelna_5 = (nauda * 0.0005) *5;
+
+document.getElementById("client").innerHTML = vards_uzvards;
+document.getElementById("money").innerHTML = nauda;
+document.getElementById("procenti_1").innerHTML = pelna_1;
+document.getElementById("procenti_2").innerHTML = pelna_2;
+document.getElementById("procenti_3").innerHTML = pelna_3;
+document.getElementById("procenti_4").innerHTML = pelna_4;
+document.getElementById("procenti_5").innerHTML = pelna_5;
+
+/*const cars = ["Saab", "Volvo", "BMW"];
+*/
+
+/* Masīvs masīvā:
+
+const cars = [["BMW","Audi"], [["X3","X5"],["A2","Q7"]]];
+
+cars[1][0]
+i=0
+cars[0][i] + "markas mašīnas ir:" +cars[1][i];
+cars[0][1] + " markas mašīnas ir: " + cars[1][1];
+*/
