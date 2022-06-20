921af26fae4e475e3ba54c6e6b5cb86a442b796c
MariteAdijane <marite.adijane@inbox.lv>
2022-06-20 20:26:40 +0300

    swap variables

---
+++ Class11-20220620/script_swap_variables.js
@@ -0,0 +1,34 @@
+var a=3,
+    b=2,
+    c=1,
+    temp=null;
+
+console.log("a= "+ a + " b= "+ b + " c= " + c);
+
+if (b>a)
+    {
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    console.log("a= "+ a + " b= "+ b + " c= " + c);
+    }
+if (c>a)
+    {
+        a=a+c;
+        c=a-c;
+        a=a-c;
+    console.log("a= "+ a + " b= "+ b + " c= " + c);
+    }
+if (c>b)
+    {
+        b=b+c;
+        c=b-c;
+        b=b-c;
+    }
+
+console.log("a= "+ a + " b= "+ b + " c= " + c);
+
+   
+
+   document.getElementById("vestule_ka_ir").innerHTML = [a,b,c];
+   document.getElementById("vestule_jauna").innerHTML = [c,b,a];
