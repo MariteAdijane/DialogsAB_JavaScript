c46015ea169d9964840734f5cff0c2287cc05598
MariteAdijane <marite.adijane@inbox.lv>
2022-07-04 18:51:57 +0300

    cikls

--- Class14_20220704/script.js
+++ Class14_20220704/script.js
@@ -1,4 +1,32 @@
+/*
+
 var i = 5;
-for (var i = 0; i , 10; i++){
-  
+for (var i = 0; i < 10; i++) {
 }
+*/
+
+console.log("Skripta izpildes sākums:\n" + new Date());
+document.getElementById("manual_count").innerHTML = "1<br>2<br>3<br>4<br>";
+
+
+text = "";
+for (let i = 1; i < 5; i++) 
+    text = text + i + "<br>";
+document.getElementById("for_automatic_count_classic").innerHTML = text;
+
+text = "";
+var i = 1;
+for (; i < 5; ) {
+    text = text + i + "<br>";
+    i++;
+}
+document.getElementById("for_automatic_count_nonclassic").innerHTML = text;
+
+
+text = "";
+var i = 1;
+while (i < 5) {
+    text = text + i + "<br>";
+    i++;
+}
+document.getElementById("while_automatic_count").innerHTML = text;
