c67dd97ffd7275235a59e70e5d27c5e13ba60807
MariteAdijane <marite.adijane@inbox.lv>
2022-07-25 21:11:12 +0300

    class20-script1

---
+++ Class20_20220725/script1.js
@@ -0,0 +1,68 @@
+// https://www.w3schools.com/js/js_object_constructors.asp
+
+// Constructor function for Person objects:
+
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+    this.name = function() {
+        return this.firstName + " " + this.lastName;
+      };
+  }
+
+
+// Create two Person objects:
+
+const myFather = new Person("John", "Doe", 50, "blue");
+const myMother = new Person("Sally", "Rally", 48, "green");
+
+// Display age:
+document.getElementById("demo1").innerHTML =
+"My father is " + myFather.age + ". My mother is " + myMother.age + ".";
+
+console.log(myFather);
+
+myFather.nationality = "English";
+myFather.nationality.enumerable = false;
+console.log(myFather);
+
+myFather.name = function() {
+    return this.firstName + " " + this.lastName;
+}
+console.log(myFather.name());
+
+Object.defineProperty(myFather, "get_nationality", {
+    get: function () {
+      return this.nationality;
+    },
+  });
+console.log(myFather.get_nationality);
+
+
+/*
+Object.defineProperty(myFather, "job", {
+    value: "officer",
+    writable: true,
+    enumerable: false});
+
+for (let current_property in myFather){
+    console.log(current_property);
+}
+*/
+
+/*
+Did You Know?
+As you can see above, JavaScript has object versions of the primitive
+data types String, Number, and Boolean. But there is no reason to create
+complex objects. Primitive values are much faster:
+
+Use string literals "" instead of new String().
+Use number literals 50 instead of new Number().
+Use boolean literals true / false instead of new Boolean().
+Use object literals {} instead of new Object().
+Use array literals [] instead of new Array().
+Use pattern literals /()/ instead of new RegExp().
+Use function expressions () {} instead of new Function().
+*/
