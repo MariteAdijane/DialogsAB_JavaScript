5f6c3c53e8e220de2911a18d7bc1e789c605b7fb
MariteAdijane <marite.adijane@inbox.lv>
2022-08-15 18:49:03 +0300

    class22

--- Class21_20220727/script.js
+++ Class21_20220727/script.js
@@ -5,13 +5,20 @@ let jsWindow = window.open(
 );
 
 setTimeout(() => {
-        window.open('http://127.0.0.1:5500/Class18_20220718/index.html', 'test', features)
+    window.open('http://127.0.0.1:5500/Class15_20220706/index.html', 'test', features)
     }, 3000);
 
-setTimeout(() => {
-    jsWindow.resizeTo(500, 300)
-}, 6000);
+    setTimeout(() => {
+      /*  jsWindow.resizeTo(600, 300);// Will not work for third part resources because of CORS
+       */ 
+     jsWindow.resizeBy(-100,-100);
+    }, 6000);
 
 setTimeout(() => {
-    jsWindow.close()
-}, 9000);
+    jsWindow.close();
+        }, 9000);
+      
+setTimeout(() => {
+    window.open("http://127.0.0.1:5500/Class17_20220713/index.html", "_blank");
+        }, 12000);
+        
---
+++ Class22_20220815/script.js
@@ -0,0 +1,19 @@
+// location:
+console.log(window.location);
+console.log(document.location);
+console.log(location);
+
+// URL ("pilnā adrese")
+console.log(location.href);
+
+// protokols (http un https - datu izgūšanai caur interneta tīklu, var būt arī lolālu aplikāciju darbināšanai)
+console.log(location.protocol);
+
+// host (IT vai "vārdiskā" IP adrese)
+// ~ mājasdarbs - palaīst par DN serveriem
+console.log(location.host);
+// host satur: hostname un port
+
+// port
+// ~ mājasdarbs - palasīt par portiem un to pielietošanu (portu numuru diapazoni)
+console.log(location.host);
