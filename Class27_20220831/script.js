09f97591ee5a65a37fad6f6d3aaa4f6d275aa71f
MariteAdijane <marite.adijane@inbox.lv>
2022-08-31 19:34:11 +0300

    statuss

--- Class27_20220831/script.js
+++ Class27_20220831/script.js
@@ -1,12 +1,17 @@
 /*garais pieraksts - der visām pogām:*/
 let btn = document.querySelector('#btn');
 
-function display() {
+/*function display(event) {
     alert('It was clicked!');
+    console.log(event.type);   
+    console.log(event.bubbles);    
+    console.log(event.cancelable); //noklusēto rīcību var atcelt, statuss true - ir iespējams
+    console.log(event.currentTarget);
+    event.currentTarget.innerText = 'Ouch'
 }
+*/
 
-btn.addEventListener('click',display);
-
+//btn.addEventListener('click',display);
 
 /* SHORT STORY - attiecās uz konkrētu pogu un nevar izsaukt display() uz visu:
 let btn = document.querySelector('#btn');
@@ -18,8 +23,17 @@ btn.addEventListener('click', function(event) {
 p/s OUTPUT: 'click'
 */
 
-/*Anonīma funkcija, īsākā versija
-btn.addEventListener('click',() => {
-    alert('It was clicked!');
+//Anonīma funkcija, īsākā versija
+btn.addEventListener('click',(event) => {
+    // alert('It was clicked!');
+    console.log(event.type);  
+    console.log(event.cancelable); 
+    console.log(event.currentTarget);  
+    console.log(event.defaultPrevented);
+    console.log(event.detail);
 });
-*/
+
+//Anonīma funkcija, īsākā versija
+btn.addEventListener('mouseup',(event) => {
+    // Uzbraucot ar peli reakcijas, diezgan bezjēdzīga, ja vien redzēt, kad klients domā - klikšķināt vai nē..
+});
