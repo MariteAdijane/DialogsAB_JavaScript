c6006e5e0f18c284c068242aa2df6979c88fc9e7
MariteAdijane <marite.adijane@inbox.lv>
2022-08-31 21:06:39 +0300

    close

--- Class27_20220831/script.js
+++ Class27_20220831/script.js
@@ -24,16 +24,50 @@ p/s OUTPUT: 'click'
 */
 
 //Anonīma funkcija, īsākā versija
-btn.addEventListener('click',(event) => {
+btn.addEventListener('click',(event) => { //'mousedownn', 'mouseover', 'mouseup'
     // alert('It was clicked!');
-    console.log(event.type);  
-    console.log(event.cancelable); 
-    console.log(event.currentTarget);  
-    console.log(event.defaultPrevented);
-    console.log(event.detail);
+    console.log("event.type: " + event.type);  
+    console.log("event.cancelable: " + event.cancelable); 
+    console.log("event.currentTarget: " + event.currentTarget);  
+    console.log("event.defaultPrevented (before event.preventDefault()): " + event.defaultPrevented);
+    console.log("event.detail: " + event.detail);  
+    console.log("event.eventPhase: " + event.eventPhase); // 1 for capturing phase, 2 for target, 3 for bubbling
+    event.preventDefault();
+    console.log("event.defaultPrevented (after event.preventDefault()): " + event.defaultPrevented);
 });
 
-//Anonīma funkcija, īsākā versija
-btn.addEventListener('mouseup',(event) => {
-    // Uzbraucot ar peli reakcijas, diezgan bezjēdzīga, ja vien redzēt, kad klients domā - klikšķināt vai nē..
+// https://developer.mozilla.org/en-US/docs/Web/API/Event/currentTarget
+function hide(e){
+    let currentTarget_ = e.currentTarget;
+    //console.log(currentTarget_.tagName);
+    if (currentTarget_.tagName == 'P')
+        setTimeout(() => {currentTarget_.style.visibility = 'hidden';}, 1000);
+    else
+        setTimeout(() => {currentTarget_.style.visibility = 'hidden';}, 2000);
+    console.log("e.currentTarget: ",e.currentTarget);
+    console.log("e.target: ",e.target);
+    // When this function is used as an event handler: this === e.currentTarget
+  }
+
+const ps = document.getElementsByTagName('p');
+
+for (let i = 0; i < ps.length; i++){
+    // console: print the clicked <p> element
+    ps[i].addEventListener('click', hide, false);
+  }
+
+//document.body.addEventListener('click', hide, false);
+
+
+let link = document.querySelector('a');
+link.addEventListener('click',function(event) {
+    console.log('clicked');
+    event.preventDefault();
+    console.log(event.currentTarget.href);
+    let jsWindow = window.open(
+        event.currentTarget.href,
+        //"https://www.w3schools.com/js/js_object_prototypes.asp",
+        "test",
+        "height=600,width=800"
+      );
 });
