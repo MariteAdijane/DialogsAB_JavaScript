edc7f38b47decfa3b34f157001334a6ee451d54f
MariteAdijane <marite.adijane@inbox.lv>
2022-09-12 21:03:06 +0300

    31-2

--- Class31_20220912/script.js
+++ Class31_20220912/script.js
@@ -1,5 +1,5 @@
 // Source - http://www.quirksmode.org/js/cookies.html
-
+/*
 function setCookie(name,value,days) {
     var expires = "";
     if (days) {
@@ -29,3 +29,24 @@ var x = getCookie('ppkcookie');
 if (x) {
     [x+1]
 }
+*/
+
+
+// https://www.javascripttutorial.net/web-apis/javascript-cookies/
+/*
+expires = new Date('2022-09-12T20:40:00.000');
+
+document.cookie = `username=admin; path=/; expires=${expires.toGMTString()}`;
+
+let intervalID = setInterval(check_cookie, 1000);
+
+function check_cookie(){
+  var current = new Date();
+  console.log(current);
+  const str = document.cookie;
+  console.log(str);
+  if(str==''){
+    clearInterval(intervalID);
+  }
+}
+*/
