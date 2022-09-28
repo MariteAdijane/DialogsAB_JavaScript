7574a6323e8483b563bfdb405756d8052dc2caaf
MariteAdijane <marite.adijane@inbox.lv>
2022-09-28 19:01:29 +0300

    Login vs next page

--- Darbs202210/script.js
+++ Darbs202210/script.js
@@ -1,8 +1,45 @@
-let currentTime = new Date();
-let year = currentTime.getFullYear();
-
-function updateYear(year){
-    document.getElementById("yearPlaceholder").innerHTML = year;
+function myFunction()
+{
+    var un = document.forms["/new_url"]["Uname"].value;
+    var pw = document.forms["/new_url"]["Pass"].value;
+    if(un== "admin" && pw=="12345");
+    {
+        window.location.href="./index2.html";
+    }
+    else{
+        alert("Invalid UserName and Password")
+    }
 }
-updateYear(year + '.gada');
-alert(year + '.gads');
+
+/* TO DO LIST */
+$(".tdl-new").bind('keypress', function(e){
+    var code = (e.keyCode ? e.keyCode : e.which);
+    if(code == 13) {
+      var v = $(this).val();
+      var s = v.replace(/ +?/g, '');
+      if (s == ""){
+        return false;
+      }else{
+        $(".tdl-content ul").append("<li><label><input type='checkbox'><i></i><span>"+ v +"</span><a href='#'>–</a></label></li>");
+        $(this).val("");
+      }
+    }
+  });
+  
+  
+  $(".tdl-content a").bind("click", function(){
+    var _li = $(this).parent().parent("li");
+        _li.addClass("remove").stop().delay(100).slideUp("fast", function(){
+          _li.remove();
+        });
+    return false;
+  });
+  
+  // for dynamically created a tags
+  $(".tdl-content").on('click', "a", function(){
+    var _li = $(this).parent().parent("li");
+        _li.addClass("remove").stop().delay(100).slideUp("fast", function(){
+          _li.remove();
+        });
+    return false;
+  });
