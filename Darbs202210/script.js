f6adc147b0e912ece82d3608b40a539c532fa795
MariteAdijane <marite.adijane@inbox.lv>
2022-09-28 20:08:59 +0300

    list

--- Darbs202210/script.js
+++ Darbs202210/script.js
@@ -1,47 +1,12 @@
+/*LOGIN FORM*/
+
 function myFunction()
 {
     var un = document.forms["myForm"]["Uname"].value;
     var pw = document.forms["myForm"]["Pass"].value;
     if(un== "admin" && pw=="12345");
     {
-        window.location.href="./index2.html";
+        window.location.href="./ToDo.html";
     }
-    else(un!= "admin" && pw!="12345")
-    {
-        alert("Invalid UserName and Password")
-    }
+   
 }
-
-
-/* TO DO LIST */
-$(".tdl-new").bind('keypress', function(e){
-    var code = (e.keyCode ? e.keyCode : e.which);
-    if(code == 13) {
-      var v = $(this).val();
-      var s = v.replace(/ +?/g, '');
-      if (s == ""){
-        return false;
-      }else{
-        $(".tdl-content ul").append("<li><label><input type='checkbox'><i></i><span>"+ v +"</span><a href='#'>–</a></label></li>");
-        $(this).val("");
-      }
-    }
-  });
-  
-  
-  $(".tdl-content a").bind("click", function(){
-    var _li = $(this).parent().parent("li");
-        _li.addClass("remove").stop().delay(100).slideUp("fast", function(){
-          _li.remove();
-        });
-    return false;
-  });
-  
-  // for dynamically created a tags
-  $(".tdl-content").on('click', "a", function(){
-    var _li = $(this).parent().parent("li");
-        _li.addClass("remove").stop().delay(100).slideUp("fast", function(){
-          _li.remove();
-        });
-    return false;
-  });
