4bb4bba30719065f99047b4caf5ec4a6a4b50041
MariteAdijane <marite.adijane@inbox.lv>
2022-09-29 20:45:15 +0300

    styles done

--- Darbs202210/script.js
+++ Darbs202210/script.js
@@ -2,11 +2,18 @@
 
 function myFunction()
 {
-    var un = document.forms["myForm"]["Uname"].value;
-    var pw = document.forms["myForm"]["Pass"].value;
-    if(un== "admin" && pw=="12345");
+    var un_admin = document.forms["myForm"]["Uname"].value;
+    var pw_admin = document.forms["myForm"]["Pass"].value;
+    if(un_admin== "admin" && pw_admin=="12345");
     {
-        window.location.href="./ToDo.html";
+        window.location.href="./ToDo_admin.html";
     }
-   
+    
+    var un_marite = document.forms["myForm"]["Uname"].value;
+    var pw_marite = document.forms["myForm"]["Pass"].value;
+    if(un_marite== "marite" && pw_marite=="marite123");
+    {
+        window.location.href="./ToDo_marite.html";
+    }
+
 }
